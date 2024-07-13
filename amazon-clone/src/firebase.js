47a8ce974ec2f16f227bfcce1244a379e0d50b69
Krishna Tiwari <95644957+krishnatiwari5@users.noreply.github.com>
2024-07-13 19:03:25 +0530

    Update firebase.js

--- amazon-clone/src/firebase.js
+++ amazon-clone/src/firebase.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBFOykufwYx5C-pTGQElRfpl9RubYKQlWw",
-    authDomain: "clone-d32ee.firebaseapp.com",
-    projectId: "clone-d32ee",
-    storageBucket: "clone-d32ee.appspot.com",
-    messagingSenderId: "346030171076",
-    appId: "1:346030171076:web:ecca7ece7dc2e8b04dba91"
+    apiKey: "ENTER_YOUR_KEY",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
